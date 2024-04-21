3614f115d85ffad078af459bddd48b4170de73b9
lujiuwu <133767919+lujiuwu@users.noreply.github.com>
2024-04-21 20:01:47 +0800

    Create problem-route-data.js

---
+++ src/web_data/problem-route-data.js
@@ -0,0 +1,18 @@
+const route_data = [
+  {
+    title:'进行初步的实地考察',
+    content: '通过专业设备测量水质具体情况',
+    timestamp: '2024.3.6 20:30',
+    color: '#409EFF'
+  },
+  {
+    title:'规划治理路线',
+    content: '考察小组确定治理方法与路线',
+    timestamp: '2024.3.7 21:30',
+    color: '#409EFF'
+  }
+  
+]
+
+// 导出
+export const problemRouteData = route_data
